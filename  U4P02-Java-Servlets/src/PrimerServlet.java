340d2ea3c3bdc37524dece91f6c45c2afa21c861
Alumno <Alumno@192.168.249.124>
2018-01-16 13:34:11 +0100

    a

---
+++  U4P02-Java-Servlets/src/PrimerServlet.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimerServlet
+ */
+@WebServlet("/PrimerServlet") // Línea A
+public class PrimerServlet extends HttpServlet { // Línea B
+	private static final long serialVersionUID = 1L; // Línea C
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // Línea D
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/><title>Primer Servlet</title></head>");
+		out.println("<body><h1>Primer servlet</h1>");
+		out.println("<h3>Me llamo ...</h3>");
+		out.println("<p>Ejecución de " + request.getContextPath() + "</p>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // Línea F
+		// TODO Auto-generated method stub
+		doGet(request, response); // Línea G
+	}
+
+}
---
+++  U4P02-Java-Servlets/src/fechayhora.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class fechayhora
+ */
+@WebServlet("/fechayhora")
+public class fechayhora extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public fechayhora() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html><head><meta charset='utf-8'/><title>Fecha y Hora</title></head>");
+        Date fecha= new Date();
+        //Caso 1: obtener la fecha , hora y salida por pantalla con formato:
+        DateFormat fechahora= new SimpleDateFormat("dd/MM/yyyy HH:mm:ss ");
+        out.println("<body><h1>Fecha y Hora</h1>");
+        out.println("<p>Fecha y Hora: "+fechahora.format(fecha)+"</p>");
+        out.println("</body></html>");
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
